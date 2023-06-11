c10edfef71881eaa49302d7fe36584f65f045cb6
ziinspiration <ilham.223040013@mail.unpas.ac.id>
2023-06-12 00:39:50 +0700

    try clone

--- website/index.php
+++ website/index.php
@@ -44,7 +44,12 @@
     <h1 class="text-center mt-3 heartBeat">I Love You</h1>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <?php
+  echo 'test';
+  ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+  </script>
 </body>
 
 </html>
